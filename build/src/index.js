cee208d73c5d0da7d7f050ed289b8a4c79bcfb98
Alexander <posb00@gmail.com>
2021-11-05 14:49:37 -0400

    config package.json and nodemon.json

--- build/src/index.js
+++ build/src/index.js
@@ -1,2 +1,13 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+//Settings
+app.set('port', 3000);
+//Starting the Server
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
+});
--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,13 @@ import express from 'express';
 
 const app = express();
 
+//Settings
+
+app.set('port',4000)
+
+
+
+//Starting the Server
+app.listen(app.get('port'), ()=>{
+    console.log(`Server on port ${app.get('port')}`)
+})
